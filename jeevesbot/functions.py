dc7380b85c5a2aeded66617497ed25dbc98fc592
Peter van Arkel <mail@peterva.net>
2021-06-07 17:10:47 +0200

    fix requirements and some spacing

--- jeevesbot/functions.py
+++ jeevesbot/functions.py
@@ -8,17 +8,19 @@ import dice
 def resolve(url):
     return urllib.request.urlopen(url).url
 
-# use the dice module for rolling
+# use the dice module for rolling.
 def roll(notation):
     roll = dice.roll(notation)
     result = int(roll)
     return roll,result
 
+# check if user has admin role and output True if it's the case.
 def checkrole(roles):
     for role in roles:
         if str(role) == env.ADMIN_ROLE:
             return True
 
+# check if the source channel is in the list of channels that are watched by the bot.
 def checkchannel(channelid):
     if channelid in env.GIFCHANNELS:
         return True
