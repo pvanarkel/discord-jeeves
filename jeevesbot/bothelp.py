f6cbb87967021a8a9da89367d97c4aaad5d7228b
Peter van Arkel <mail@peterva.net>
2021-06-15 00:43:38 +0200

    fix help function

--- jeevesbot/bothelp.py
+++ jeevesbot/bothelp.py
@@ -1,20 +1,19 @@
-keywords = ['gifbot']
+keywords = ['roll']
 
 def help(keyword=''):
     """Filter for general help or specific keyword help."""
     if not keyword:
         file = open('help/help.md')
         contents = file.read()
-        return (contents)
+        return format((contents))
     if keyword[0]:
         keyword = str.lower(keyword)
         if keyword in keywords:
             document = 'help/help_' + keyword + '.md'
-            print(document)
             file = open(document, 'r')
             contents = file.read()
-            return (contents)
+            return format((contents))
         else:
             file = open('help/help.md')
             contents = file.read()
-            return (contents)
+            return format((contents))
