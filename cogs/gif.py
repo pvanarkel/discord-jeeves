0e889261a0e4f9c1ac7c05f43f6df99e16e0c76d
Peter van Arkel <mail@peterva.net>
2021-07-15 22:17:05 +0200

    refactor code to use cogs instead of loose functions

---
+++ cogs/gif.py
@@ -0,0 +1,69 @@
+import discord
+from discord.ext import commands
+import logging
+from jeevesbot import functions
+
+e = discord.Embed()
+
+# setup logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('jeeves')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='jeeves.log', encoding='utf-8', mode='a')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+
+class Gif(commands.Cog):
+
+
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.content.startswith('https://tenor.com/'):
+            roles = functions.checkrole(message.author.roles)
+            channel = functions.checkchannel(message.channel.id)
+            embed_url = message.content
+            follow_url = embed_url + '.gif'
+            full_url = await functions.resolve(follow_url)
+            gif_url = full_url.split('?')[0]
+            embed = e.set_image(url=gif_url)
+            if channel is True:
+                if roles is not True:
+                    await message.channel.send(embed=embed)
+                    logline = (str(message.author) + ' requested a gif: ' + str(gif_url))
+                    logger.info(logline)
+        if message.content.endswith('.gif'): 
+            roles = functions.checkrole(message.author.roles)
+            channel = functions.checkchannel(message.channel.id) 
+            embed_url = message.content
+            embed = e.set_image(url=embed_url)
+            if channel is True:
+                if roles is not True:
+                    await message.channel.send(embed=embed)
+                    logline = (str(message.author) + ' requested a gif: ' + str(embed_url))
+                    logger.info(logline)       
+        if message.content.startswith('https://giphy.com/'):
+            roles = functions.checkrole(message.author.roles)
+            channel = functions.checkchannel(message.channel.id)
+            embed_url = message.content
+            image_code = embed_url.split('-')[-1]
+            gif_url = 'https://media.giphy.com/media/' + image_code + '/giphy.gif'
+            embed = e.set_image(url=gif_url)        
+            if channel is True:
+                if roles is not True:
+                    await message.channel.send(embed=embed)
+                    logline = (str(message.author) + ' requested a gif: ' + str(gif_url))
+                    logger.info(logline)
+
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('##### GIF module active')
+
+
+def setup(bot):
+    bot.add_cog(Gif(bot))
